e44d6514d999b9493608ab7da5499b954956c2a8
mastermindclasses0001-lab <mastermindclasses0001@gmail.com>
2026-09-14 23:26:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import flet as ft
 
+def main(page: ft.Page):
+    page.title = "BhrivimLab"
+    page.padding = 0
+    my_url = "https://www.bhrivimlab.com/"
+    wv = ft.WebView(url=my_url, expand=True)
+    page.add(wv)
+
+ft.app(target=main)
