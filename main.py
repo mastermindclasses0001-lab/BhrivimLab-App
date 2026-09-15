b5ca9e30f4da8265372165d24e27273db1abc502
mastermindclasses0001-lab <mastermindclasses0001@gmail.com>
2026-09-15 20:18:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,14 @@ import flet as ft
 import flet_webview as fwv
 
 def main(page: ft.Page):
-    # App ka naam aur thodi settings
     page.title = "BhrivimLab"
-    page.padding = 0  # Screen ke edges par khali space hatane ke liye
     
-    # WebView widget banana
     wv = fwv.WebView(
-        url="https://www.bhrivimlab.com",  # <-- YAHAN APNI WEBSITE KA ASLI LINK DAALEIN
+        url="https://www.bhrivimlab.com",  # <-- YAHAN APNI ASLI WEBSITE KA LINK DAALEIN
         expand=True
     )
     
-    # Screen par WebView ko dikhana
     page.add(wv)
 
-ft.app(target=main)
+# Naya command jo V1 me officially use hota hai
+ft.run(main)
